209c6f94bcfa2e653a9cdc16f07079f7199424ce
steveglowplunk <28670916+steveglowplunk@users.noreply.github.com>
2024-04-03 18:56:40 +0800

    Profile page mockup

--- frontend/app/(main)/profile/[[...username]]/page.tsx
+++ frontend/app/(main)/profile/[[...username]]/page.tsx
@@ -1,43 +1,75 @@
-import React from 'react'
+"use client";
+
+import React, { useEffect, useRef, useState } from 'react'
 import { Image } from 'primereact/image';
-import { object } from 'zod';
-import { Container } from 'postcss';
 import { Avatar } from 'primereact/avatar';
-import { AvatarGroup } from 'primereact/avatargroup';
+import PostBox from "@/components/home/PostBox";
+import dummyPost from "@/dummy_data/dummyPosts.json";
+import { Button } from 'primereact/button';
+
 
 const profile = ({ params }: { params: { username: string } }) => {
   let profileUsername = params.username;
   if (!profileUsername) {
     profileUsername = "self";
   }
+
+  const divRef = useRef<HTMLDivElement>(null);
+  const [marginTop, setMarginTop] = useState(0);
+
+  useEffect(() => {
+    if (divRef.current) {
+      setMarginTop(divRef.current.clientHeight - 70);
+    }
+  }, []);
+
+  const [selectedButton, setSelectedButton] = useState('Posts');
   return (
-    <div className='flex w-full flex-col justify-start'>
-      <div className='w-4/5'>
-         
-        <div className='flex h-72 w-full overflow-hidden  bg-gray-300 '  > 
-        
-          <img className='object-cover max-h-72 maxw-full ' src="/placeholder_profilePic.png" alt="profile background pic"/>
-            
-        </div>
-
-        <div className='flex items-center justify-between'>
-          <div className='flex items-center gap-3'>
-            <Avatar image="/placeholder_profilePic.png" size="xlarge" shape="circle" className='shadow-2xl object-cover'/>
-            <div className='flex-1'>
-              <h2 className='text-left font-bold text-white'>
-                {profileUsername} 
-              </h2>
-              <p className='text-base text-gray-500'>
-
-              </p>
+    <div className='flex w-full h-full flex-col overflow-y-auto overflow-x-clip'>
+      <div className='flex h-72 w-full bg-[#e5eeee] relative'> 
+        <Image className='z-0' 
+        src="/art-rachen-yJpjLD3c9bU-unsplash.jpg" alt="profile background pic"
+            pt={{
+              image: {className: "object-cover h-full w-full"},
+              previewContainer: {className: "z-20"}
+            }}
+            preview
+        />
+        <div ref={divRef} className='absolute top-full transform translate-x-10 -translate-y-[4.5rem] z-10'>
+          <div className='flex mb-4'>
+            <Image
+                src='/anh-meme-cheems-hmmm.jpg'
+                alt='profile pic'
+                pt={{
+                  image: {className: "rounded-full h-36 w-36 object-cover"},
+                  previewContainer: {className: "z-20"}
+                }}
+                preview />
+            <div className="flex flex-col min-h-full justify-end ml-2">
+              <p className='text-3xl'>Cheem</p>
+              <p className='text-xl text-gray-600'>@cheemuser</p>
             </div>
           </div>
-            
+            <p className='text-xl whitespace-pre-wrap max-w-96'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         </div>
       </div>
-      <div className='w-1/5'></div>
-    </div>
+      <div style={{ marginTop: `${marginTop}px` }}>
+        <div className='flex justify-between w-full'>
+          <Button className={`w-full ${selectedButton === 'Posts' ? 'border-0 !border-b-2 border-orange1' : ''}`} 
+          label="Posts" text
+          onClick={() => setSelectedButton('Posts')} />
+          <Button className={`w-full ${selectedButton === 'Replies' ? 'border-0 !border-b-2 border-orange1' : ''}`}
+          label="Replies" text
+          onClick={() => setSelectedButton('Replies')} />
+        </div>
 
+      </div>
+
+      {/* <div className="bg-slate-500 h-96 w-10"></div> */}
+      {dummyPost.map((post, index) => <PostBox key={index} {...post} />)}
+
+      
+    </div>
      
   )
 }
