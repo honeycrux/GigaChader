4b31700961dde67b57483c91fc98c42567f5fd93
dmdmtommy <1155175256@link.cuhk.edu.hk>
2024-04-08 18:10:18 +0800

    add repost function

--- frontend/components/home/PostBox.tsx
+++ frontend/components/home/PostBox.tsx
@@ -8,54 +8,86 @@ import { InputTextarea } from 'primereact/inputtextarea';
 import { formatDate } from "@/lib/utils";
 import Link from "next/link";
 import { apiClient } from "@/lib/apiClient";
-
+import { Dialog } from 'primereact/dialog';
 interface Props {
     id: string;
     currentUserName: string;  // for checking if the current user can delete a post
     parentPostId: string | null; // for comments
+    repostingPostId?: string | null; // for reposts
     content: string;
     author: {
-      username: string;
-      displayName: string;
-      imageUrl: string;
-      id: string;
+        username: string;
+        displayName: string;
+        imageUrl: string;
+        id: string;
     };
     createdAt: string;
     isComment?: boolean;
     commentCount: number;
     likeCount: number;
     bVisitParentPost?: boolean;
-  }
+    bButtonvisible?: boolean;
+    bshowBorder?: boolean;
+}
 
 const PostBox = (
     {
         id,
         currentUserName,
         parentPostId,
+        repostingPostId,
         content,
         author,
         createdAt,
         isComment,
         commentCount,
         likeCount,
-        bVisitParentPost
-      }: any) => {
+        bVisitParentPost,
+        bButtonvisible = true,
+        bshowBorder = false
+    }: Props) => {
     const toast = useRef<Toast>(null);
     const [heartPath, setHeartPath] = useState("/heart.svg");
     const bisLiked = useRef(false);
     const [likeCountInternal, setlikeCountInternal] = useState(likeCount);
 
+    const parentPostInfo = {
+        id,
+        currentUserName,
+        parentPostId,
+        repostingPostId,
+        content,
+        author,
+        createdAt,
+        isComment,
+        commentCount,
+        likeCount,
+        bVisitParentPost,
+        bButtonvisible,
+        bshowBorder
+    }
+    
+    const [parentPost, setParentPost] = useState<any>();
+
+    const getParentpostInfo = async() => {
+        if (repostingPostId) {
+            const res = await apiClient.post.getPost({ params: { postId: repostingPostId } });
+            console.log(res.body);
+            setParentPost(res.body);
+        }
+    };
+
     const checkLiked = async () => {
         const res = await apiClient.post.getLikes({ query: { postId: id } });
         // bisLiked.current = res.body;
-        // console.log("post content:");
-        // console.log(content);
+        //console.log("post content:");
+        //console.log(content);
         // console.log(res.body);
         // @ts-ignore
         bisLiked.current = res.body.some((user: { username: string }) => user.username === currentUserName);
         // @ts-ignore
         setlikeCountInternal(res.body.length);
-        
+
         if (bisLiked.current) {
             setHeartPath("/heart_red.svg");
         } else {
@@ -64,9 +96,14 @@ const PostBox = (
     }
 
     useEffect(() => {
-        
         checkLiked();
     }, [id]);
+
+    useEffect(() => {
+        if (repostingPostId) {
+            getParentpostInfo();
+        }
+    }, [repostingPostId]);
 
     const handleHeartClick = async () => {
         bisLiked.current = !bisLiked.current;
@@ -75,7 +112,7 @@ const PostBox = (
         } else {
             setHeartPath("/heart.svg");
         }
-        const res = await apiClient.post.postLike({ body: { postId: id, set: bisLiked.current }});
+        const res = await apiClient.post.postLike({ body: { postId: id, set: bisLiked.current } });
         console.log(res);
         checkLiked();
     }
@@ -91,57 +128,100 @@ const PostBox = (
             toast.current.show({ severity: "info", summary: "Success", detail: "Fake submit comment" });
         }
     }
-  return (
-    <div className="flex flex-col w-full bg-orange2 rounded-2xl p-4">
-        <Toast ref={toast}></Toast>
-        <div className="flex">
-            <div className="flex flex-col items-center">
-                <Avatar className="mr-2" image="/cheem.jpg" shape="circle" size="large" />
-                <div className="relative mt-2 grow w-0.5 rounded-full bg-gray-600" />
-            </div>
-            <div className="whitespace-pre-wrap">
-                <div>
-                    <Link className="mr-2 hover:underline" href={`/profile/${author.username}`}>{author.displayName} &nbsp;
-                        <span className="text-gray-600">@{author.username}</span>
-                    </Link>
+
+    const [visible, setVisible] = useState(false);
+    const [postContent, setPostContent] = useState<string>('');
+    const handleRepostClick = () => {
+        setVisible(true);
+    }
+    const handleRepostSubmit = async () => {
+
+        const res = await apiClient.post.postCreate({
+            body: {
+                content: postContent,
+                repostingPostId: id,
+            },
+        });
+        console.log(res);
+        setVisible(false);
+    }
+    const dialogFooter = (
+        <div className="p-d-flex p-jc-end">
+            <Button label="Submit" className="w-fit" onClick={handleRepostSubmit} />
+        </div>
+    );
+
+    return (
+        <div className="flex flex-col w-full bg-orange2 rounded-2xl p-4">
+            <Toast ref={toast}></Toast>
+            <div className="flex">
+                <div className="flex flex-col items-center">
+                    <Avatar className="mr-2" image="/cheem.jpg" shape="circle" size="large" />
+                    <div className="relative mt-2 grow w-0.5 rounded-full bg-gray-600" />
                 </div>
-                <div>
-                    <p>{content}</p>
-                </div>
-                <div className="inline-flex [&>*]:mr-2 items-end">
-                    {currentUserName && (<Image src={heartPath}
+                <div className="whitespace-pre-wrap">
+                    <div>
+                        <Link className="mr-2 hover:underline" href={`/profile/${author.username}`}>{author.displayName} &nbsp;
+                            <span className="text-gray-600">@{author.username}</span>
+                        </Link>
+                    </div>
+                    <div>
+                        <p>{content}</p>
+                        {(repostingPostId && parentPost) && (
+                            <div className="border border-gray-500 rounded-lg m-2">
+                                <PostBox {...parentPost} bButtonvisible={true} currentUserName={currentUserName} />
+                            </div>
+                        )}
+                    </div>
+                    {bButtonvisible && (<div className="inline-flex [&>*]:mr-2 items-end">
+                        {currentUserName && (<Image src={heartPath}
                             width="24"
                             alt="heart"
                             onClick={handleHeartClick}
                             className="cursor-pointer"
-                    />)}
-                    {/* <Link href={bVisitParentPost && parentPostId ? `/post/${parentPostId}` : `/post/${id}`}> */}
-                    <Link href={`/post/${id}`}>
-                    <Image src="/comment.svg"
+                        />)}
+                        {/* <Link href={bVisitParentPost && parentPostId ? `/post/${parentPostId}` : `/post/${id}`}> */}
+                        <Link href={`/post/${id}`}>
+                            <Image src="/comment.svg"
+                                width="24"
+                                alt="comment"
+                                className="cursor-pointer"
+                            />
+                        </Link>
+                        <Image src="/retweet-round.svg"
                             width="24"
-                            alt="comment"
+                            alt="repost"
+                            onClick={handleRepostClick}
                             className="cursor-pointer"
-                    />
-                    </Link>
-                    <span className="text-gray-600 text-sm">
-                        {likeCountInternal} like(s), {commentCount} comment(s)
-                    </span>
+
+                        />
+                        <span className="text-gray-600 text-sm">
+                            {likeCountInternal} like(s), {commentCount} comment(s)
+                        </span>
+                    </div>)}
                 </div>
             </div>
+            <p className="text-gray-600">{formatDate(createdAt)}</p>
+            {parentPostId && <Link href={`/post/${parentPostId}`} className="text-gray-600 hover:underline">Go to parent post</Link>}
+            {
+                bShowCommentBox &&
+                <div className="flex flex-col w-full">
+                    <p className="text-xl">Comment</p>
+                    <InputTextarea value={commentContent} onChange={(e) => setCommentContent(e.target.value)}
+                        rows={4} cols={30} autoResize className="mb-2" />
+                    <Button label="Submit" className="w-fit" onClick={handleCommentSubmit} />
+                </div>
+            }
+
+
+            <Dialog header="Repost" visible={visible} style={{ width: '50vw' }} onHide={() => setVisible(false)} footer={dialogFooter}>
+                <InputTextarea className="w-full" value={postContent} onChange={(e) => setPostContent(e.target.value)}
+                    rows={6} autoResize />
+               
+                <PostBox {...parentPostInfo} bButtonvisible={false} />
+            </Dialog>
         </div>
-        <p className="text-gray-600">{formatDate(createdAt)}</p>
-        { parentPostId && <Link href={`/post/${parentPostId}`} className="text-gray-600 hover:underline">Go to parent post</Link> }
-        {
-            bShowCommentBox &&
-            <div className="flex flex-col w-full">
-                <p className="text-xl">Comment</p>
-                <InputTextarea value={commentContent} onChange={(e) => setCommentContent(e.target.value)}
-                rows={4} cols={30} autoResize className="mb-2" />
-                <Button label="Submit" className="w-fit" onClick={handleCommentSubmit} />
-            </div>
-        }
-    </div>
-  )
+    )
 }
 
 export default PostBox
